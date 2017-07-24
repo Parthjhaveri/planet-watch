290041a5c7fafaff4ac1390e1069209bf2b68238
Parthjhaveri <parthjhaveri209@gmail.com>
2017-07-24 02:31:17 -0400

    neo page

--- src/neotrack.js
+++ src/neotrack.js
@@ -6,7 +6,11 @@ import Footer from './footer.js';
 import $ from 'jquery'; 
 import { BrowserRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import Main from './Main.js';;
+import Main from './Main.js';
+
+// REACT VIDEO
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
 
 
 class NeoTracker extends Component {
@@ -16,7 +20,7 @@ class NeoTracker extends Component {
     super(props);
 
     this.state = {
-      text: "NEO TRACKER!"
+      videoURL: "./images/asteroidvid.mp4"
     }
 
   }
@@ -27,11 +31,30 @@ class NeoTracker extends Component {
 
         <Navi />
 
-        {this.state.text}
+        {
+          // MAIN BANNER
+        }
+
+        <div className="nasawallpaper">
+
+          <center>
+            <ul className="staticnav" id="astnav">
+              <li className="aststaticnavli"><Link to='/'>HOME</Link></li>
+              <li className="aststaticnavli"><Link to='/neo-tracker'>NEO TRACKER</Link></li>
+              <li className="aststaticnavli">POLLUTION TRACKER</li>
+              <li className="aststaticnavli">GEOPOLITICAL NEWS</li>
+              <li className="aststaticnavli">OCEAN CLEANUP</li>
+            </ul>
+          </center>
+
+          <p id="asttrackerheading">ASTEROID TRACKER</p>
+
+        </div>
         
         {
           //new Date().toLocaleTimeString()
         }
+
 
         <Footer />
 
